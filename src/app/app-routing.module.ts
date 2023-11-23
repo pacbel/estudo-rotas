1ec5db62aa183e0aeac0f64c09990d120386e420
Carlos Pacheco <suporte@sistemavirtual.com.br>
2023-11-23 18:24:09 -0300

    Estudo Rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Content1Component } from './pages/content1/content1.component';
+import { PagesComponent } from './pages/pages.component';
+import { Content2Component } from './pages/content2/content2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: PagesComponent,
+    children: [
+      { path: 'content1', component: Content1Component },
+      { path: 'content2', component: Content2Component },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsModule } from './components/components.module';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
